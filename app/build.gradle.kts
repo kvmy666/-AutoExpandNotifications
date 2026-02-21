a0b9c30c0deaff1f656ad288433929b6d37233ef
kvmy666 <krom306@gmail.com>
2026-02-21 17:24:24 +0300

    تحديث جديد وإصدار نسخة 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.autoexpand.xposed"
         minSdk = 35
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.4"
     }
 
     signingConfigs {
